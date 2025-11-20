38f3b542873614185e7d035638a087ed0de38b42
Kik02 <Golovin.Savelii@yandex.ru>
2025-11-20 15:22:49 +0500

    Создание метода AutorizationUser и создание переменных.Черных

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class Program
+    class Program
     {
+        public static List<User> Users = new List<User>();
+        public static IPAddress IpAdress;
+        public static int Port;
+
         static void Main(string[] args)
         {
         }
+
+        public static bool AutorizationUser(string login, string password)
+        {
+            User user = null;
+            user = Users.Find(x => x.login == login && x.password == password);
+            return user != null;
+        }
+
     }
 }
