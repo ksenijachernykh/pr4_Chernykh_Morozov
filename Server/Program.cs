364487076d32b9c076da0e92e658e1935e736410
Kik02 <Golovin.Savelii@yandex.ru>
2025-11-20 15:25:41 +0500

    Создание метода GetDirectory.Черных

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -23,6 +24,29 @@ namespace Server
             user = Users.Find(x => x.login == login && x.password == password);
             return user != null;
         }
+
+        public static List<string> GetDirectory(string src)
+        {
+            List<string> FoldersFiles = new List<string>();
+            if (Directory.Exists(src))
+            {
+                string[] dirs = Directory.GetDirectories(src);
+                foreach (string dir in dirs)
+                {
+                    string NameDirectory = dir.Replace(src, "");
+                    FoldersFiles.Add(NameDirectory + "/");
+                }
+                string[] files = Directory.GetFiles(src);
+                foreach (string file in files)
+                {
+                    string NameFile = file.Replace(src, "");
+                    FoldersFiles.Add(NameFile);
+
+                }
+            }
+            return FoldersFiles;
+        }
+
 
     }
 }
